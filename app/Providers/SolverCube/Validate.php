a38fe5f8d700afd3fb54163d4f1435b5cb5122c7
Daniel Villanueva <daniel.jose@villanueva.com.ve>
2015-11-24 15:22:02 -0430

    Add Validacion de Operaciones del Cubo

--- app/Providers/SolverCube/Validate.php
+++ app/Providers/SolverCube/Validate.php
@@ -4,8 +4,9 @@ class Validate {
 		
 	private $arrayLines;
 	private $respose;
-	private $currentLine;
-	private $currentOper;
+	private $currentLine = 0;
+	private $currentTest = 0;
+	private $currentOper = 0;
 	private $T;
 	private $N;
 	private $M;
@@ -39,15 +40,26 @@ class Validate {
 					if(!$this->validTestCases($textSpace)) {
 						return $this->respose; 
 					}
-				} else if($i == 1) {
-					// N M
-					if(!$this->validNM($textSpace)) {
-						return $this->respose;
+					$this->inN = true;
+				} else if($i != 0) {
+					if($this->inN) {
+						// N M
+						if(!$this->validNM($textSpace)) {
+							return $this->respose;
+						}
+						$this->inN = false;
+					} else if($this->currentOper <= $this->M) {
+						// Operation
+						if(!$this->validOperation($textSpace)) {
+							return $this->respose;
+						}
+					} else {
+						$this->currentOper = 0;
+						$this->inN = true;
 					}
 					$this->respose["warning"] = ($this->currentOper < $this->M);
 				}
 			}
-			$this->respose["warning"] = (count($this->arrayLines) < 1 || count($this->arrayLines) < 2);
 		} else {
 			$this->respose["error"] 	= true;
 			$this->respose["errorInfo"] = "Input Vacio";
@@ -71,21 +83,83 @@ class Validate {
 	}
 	private function validNM($arrayText) {
 		// Validate 1 <= N <= 100 and 1 <= M <= 1000 
-		if(count($arrayText) > 2 || count($arrayText) < 2 || !is_numeric($arrayText[0]) || !is_numeric($arrayText[1])) {
+		if(count($arrayText) > 2 || !is_numeric($arrayText[0]) || (isset($arrayText[1]) && !is_numeric($arrayText[1]))) {
 			$this->respose["error"] 	= true;
 			$this->respose["errorInfo"] = "Solo dos Numero Entero separados por un Espacios";
 			return false;
 		}
+        $this->respose["warning"] = (count($arrayText) < 2);
 		$this->N = (int) $arrayText[0];
-		$this->M = (int) $arrayText[1];
 		if($this->N <= 1 || $this->N >= 100) {
 			$this->respose["error"] 	= true;
 			$this->respose["errorInfo"] = "El primer entero debe ser Mayor a 1 y Menor a 100";
 			return false;
-		} else if($this->M <= 1 || $this->M >= 1000) {
+		}
+		if(!$this->respose["warning"]) {
+			$this->M = (int) $arrayText[1]; 
+			if($this->M < 1 || $this->M > 1000) {
+				$this->respose["error"] 	= true;
+				$this->respose["errorInfo"] = "El segundo entero debe ser Mayor a 1 y Menor a 1000";
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private function validOperation($arrayText) {
+		$arrayText[0] = strtoupper($arrayText[0]);
+		$arrOperU 	  = str_split("UPDATE",strlen($arrayText[0]));
+		$arrOperQ	  = str_split("QUERY",strlen($arrayText[0]));
+		$textPosU	  = stripos("UPDATE",$arrayText[0]);
+		$textPosQ	  = stripos("QUERY",$arrayText[0]);
+		if(($textPosU === false) && ($textPosQ === false)) {
 			$this->respose["error"] 	= true;
-			$this->respose["errorInfo"] = "El segundo entero debe ser Mayor a 1 y Menor a 1000";
+			$this->respose["errorInfo"] = "Las operaciones solo puede ser UPDATE y/o QUERY";
 			return false;
+		}
+		if((!($arrOperU[$textPosU] == $arrayText[0])) && (!($arrOperQ[$textPosQ] == $arrayText[0]))) {
+			$this->respose["error"] 	= true;
+			$this->respose["errorInfo"] = "Las operaciones solo puede ser UPDATE y/o QUERY";
+			return false;
+		}
+		if($arrayText[0]=='UPDATE') {
+			if(!$this->validUpdate($arrayText)) {
+				return false;
+			}
+		} else if($arrayText[0]=='QUERY') {
+			if(!$this->validQuery($arrayText)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	private function validUpdate($arrayText) {
+		$this->respose["warning"] = (count($arrayText) < 5);
+		if(!$this->respose["warning"]) {
+			for($u = 1; $u < (count($arrayText)-1); $u++) {
+				if(!is_numeric($arrayText[$u]) || $arrayText[$u] < 1 || $arrayText[$u] > $this->N) {
+					$this->respose["error"] 	= true;
+					$this->respose["errorInfo"] = "Formato: UPDATE x y z w - cordenadas del cubo y el valor";
+					return false;
+				} else if($u == ((count($arrayText)-1)-1)) {
+					$this->currentOper++;
+				}
+			}
+		}
+		return true;
+	}
+	private function validQuery($arrayText) {
+		$this->respose["warning"] = (count($arrayText) < 7);
+		if(!$this->respose["warning"]) {
+			for($q = 1; $q < count($arrayText); $q++) {
+				if(!is_numeric($arrayText[$q]) || $arrayText[$q] < 1 || $arrayText[$q] > $this->N) {
+					$this->respose["error"] 	= true;
+					$this->respose["errorInfo"] = "Formato: QUERY x y z x1 y1 z1 - cordenadas del cubo desde-hasta";
+					return false;
+				} else if($q == (count($arrayText)-1)) {
+					$this->currentOper++;
+				}
+			}
 		}
 		return true;
 	}
